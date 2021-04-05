40bef38f660dcf8b5c2e7c5bf0d22411f4d18bd9
Ivan Ogasawara <ivan.ogasawara@gmail.com>
2021-04-05 09:22:58 -0400

    Use conda on CI (#38)
    * Use conda on CI
    
    * Pin sqlalchemy
    
    * Fix centroid test
    
    * Use mamba
    
    * Fix pre-commit

--- ibis_omniscidb/tests/test_geo.py
+++ ibis_omniscidb/tests/test_geo.py
@@ -1,5 +1,3 @@
 def test_centroid(geo_table):
-    assert (
-        'POINT (25.4545454545455 26.969696969697)'
-        == geo_table.geo_polygon.centroid().execute().values[0]
-    )
+    result = geo_table.geo_polygon.centroid().execute()
+    assert 'POINT (25.4545454545455 26.969696969697)' == result.values[0].wkt
