5bbbbe909151fc25cd876b26eb4afc6a16a479d6
Ivan Ogasawara <ivan.ogasawara@gmail.com>
2021-09-20 16:24:30 -0400

    Remove unnecesary code

--- ibis_omniscidb/tests/test_geo.py
+++ ibis_omniscidb/tests/test_geo.py
@@ -1,10 +1,3 @@
 def test_centroid(geo_table):
     result = geo_table.geo_polygon.centroid().execute()
     assert 'POINT (25.4545454545455 26.969696969697)' == result.values[0].wkt
-
-
-def test_equals(geo_table):
-    expr = geo_table.geo_point == geo_table.geo_point
-    raise Exception(expr.compile())
-
-    assert expr.execute().all()
